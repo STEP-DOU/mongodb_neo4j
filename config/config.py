afd010028aa324f327e40b94d7e5390fe832b803
STEP-DOU <stephanedoungue@gmail.com>
2025-04-02 22:43:20 -0400

    creation du fichier config et configuration de la connexion a mongodb et neo4j

---
+++ config/config.py
@@ -0,0 +1,9 @@
+# config/config.py
+
+# MongoDB Atlas URI
+MONGO_URI = "mongodb+srv://stephanedoungue:15102935@cluster0.ejocy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Neo4j or local
+NEO4J_URI = "bolt://44.222.182.146"  # ou bolt://<hôte>:<port>
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "junk-instruction-wounds"
